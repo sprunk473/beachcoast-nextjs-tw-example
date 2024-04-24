3738233c2a5b5b7197a4e9be92819f15f9776492
sprunk <158991746+sprunk473@users.noreply.github.com>
2024-04-24 12:29:31 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,69 @@
+import React from 'react';
 import { Inter } from "next/font/google";
-import React from "react";
-import Link from "next/link";
-import { Typewriter } from "react-simple-typewriter";
-import Head from 'next/head';
+import { EnvelopeIcon, KeyIcon, PaperAirplaneIcon } from '@heroicons/react/24/solid';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return (
-    <div className={`min-h-screen bg-zinc-800 ${inter.className} relative justify-center items-center flex`}>
-      <Head>
-        <title>incompetent.dev</title>
-        <meta name="description" content="private solutions" />
-        <meta property="og:image" content="incomp.png" />
-        <link rel="icon" href="incomp.png" />
-        <meta property="og:title" content="incompetent.dev" />
-        <meta property="og:description" content="private solutions" />
-        <meta property="og:image" content="incomp.png" />
-      </Head>
-      <div className="relative overflow-hidden">
-        <main className="">
-          <div className="text-center">
-            <span className="text-4xl md:text-4xl font-bold">
-              <span className="bg-gradient-to-r from-emerald-300 to-emerald-600 text-transparent bg-clip-text">
-                incompetent.dev
-              </span>
-            </span>
-            <h1 className="md:text-lg justify-center tracking-tight font- text-gray-900 mb-4 mt-2 inline flex">
-              <span className="block text-zinc-300">creating solutions for your </span>{' '}
-              <div className="block text-emerald-500 ml-1">
-                <Typewriter
-                  words={['fivem server', 'discord server']}
-                  loop={false}
-                  cursor
-                  cursorStyle='_'
-                  typeSpeed={100}
-                  deleteSpeed={65}
-                  delaySpeed={1000}
-                />
-              </div>
-            </h1>
-            <div className="flex justify-center space-x-2">
-              <Link href="https://github.com/incompetentdevs" className="py-1 px-4 group flex items-center justify-center hover:scale-95 bg-zinc-700 hover:bg-zinc-700 transition duration-150 rounded-3xl text-zinc-200 font-medium">
-                <img src="github3.png" alt="Game Panel Icon" className="w-5 mr-2 pointer-events-none" draggable="true" />
-                github
-              </Link>
+function LoginPage() {
+    return (
+        <div className={` ${inter.className}`}>
+            <div className="min-h-screen flex items-center justify-center bg-zinc-700">
+                <div className="max-w-md w-full bg-zinc-800 p-8 rounded shadow-md">
+                    <h2 className="text-2xl font-semibold text-center mb-4 text-zinc-300">Portal Login</h2>
+                    <form>
+                        <div className="relative mb-4">
+                            <label htmlFor="email" className="block text-zinc-200 font-medium mb-2">Email</label>
+                            <div className="relative">
+                                <input type="email" id="email" name="email" className="text-zinc-200 bg-zinc-600 w-full px-4 py-2 border-2 border-zinc-400 rounded-md focus:outline-none focus:border-blue-500" />
+                                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                                    <EnvelopeIcon className="w-5 text-zinc-400" />
+                                </div>
+                            </div>
+                        </div>
 
-              <Link href="https://discord.gg/V7zPfHM5Ka" className="py-1 px-4 group flex items-center justify-center hover:scale-95  bg-zinc-600 hover:bg-zinc-700 transition duration-150 rounded-3xl text-zinc-200 font-medium">
-              <img src="discord-mark-white.png" alt="Game Panel Icon" className="w-5 mr-2 pointer-events-none" draggable="true" />
-                discord
-              </Link>
+                        <div className="relative mb-6">
+                            <label htmlFor="password" className="block text-zinc-200 font-medium mb-2">
+                                Password
+                            </label>
+                            <div className="relative">
+                                <input type="password" id="password" name="password" className="text-zinc-200 bg-zinc-600 w-full px-4 py-2 border-2 border-zinc-400 rounded-md focus:outline-none focus:border-blue-500" />
+                                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                                    <KeyIcon className="w-5 text-zinc-400" />
+                                </div>
+                            </div>
+                        </div>
 
-              <Link href="https://pterodactyl.incompetent.dev/" className="py-1 px-4 group flex items-center justify-center hover:scale-95  bg-emerald-600 hover:bg-emerald-700 transition duration-150 rounded-3xl text-zinc-950 font-medium">
-                game panel
-              </Link>
+                        <button type="submit" className="w-full bg-green-700 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-green-800 focus:outline-none">
+                            <PaperAirplaneIcon className="w-5 mr-2" />
+                            Login
+                        </button>
+
+                        <div className="mt-6">
+                            <div className="relative">
+                                <div className="absolute inset-0 flex items-center">
+                                    <div className="w-full border-t border-zinc-300" />
+                                </div>
+                                <div className="relative flex justify-center text-sm">
+                                    <span className="px-2 bg-zinc-800 text-zinc-400 font-light">Or continue with</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col space-y-4 mt-4">
+                            <button type="button" className="bg-indigo-500 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-indigo-600 focus:outline-none focus:bg-gray-700">
+                                <img src="discord-mark-white.png" alt="Discord Icon" className="w-6 h-6 mr-2 object-contain" />
+                                Discord
+                            </button>
+                            <button type="button" className="bg-blue-800 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-blue-900 focus:outline-none focus:bg-gray-700">
+                                <img src="steam.png" alt="Discord Icon" className="w-6 h-6 mr-2 object-contain" />                Steam
+                            </button>
+                        </div>
+                    </form>
+                    <p className="text-center text-zinc-400 text-sm mt-4">© 2024 Beachcoast. All rights reserved.</p>
+                </div>
             </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
+
+export default LoginPage;
