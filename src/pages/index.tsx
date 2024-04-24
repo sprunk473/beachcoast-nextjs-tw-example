20733d431df96602944c999b59ce4ad0e4cf4307
sprunk473 <official.scythestudios@gmail.com>
2024-04-24 12:44:10 -0400

    update discord button color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,17 +48,17 @@ function LoginPage() {
                                     <div className="w-full border-t border-zinc-300" />
                                 </div>
                                 <div className="relative flex justify-center text-sm">
-                                    <span className="px-2 bg-zinc-800 text-zinc-400 font-light">Or continue with</span>
+                                    <span className="px-2 bg-zinc-800 text-zinc-400">Or continue with</span>
                                 </div>
                             </div>
                         </div>
 
                         <div className="flex flex-col space-y-4 mt-4">
-                            <button type="button" className="bg-indigo-500 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-indigo-600 focus:outline-none">
+                            <button type="button" className="bg-[#5865F2] text-white px-4 py-2 rounded-md flex items-center justify-center focus:outline-none">
                                 <img src="logos/discord.png" alt="Discord Icon" className="w-6 h-6 mr-2 object-contain" />
                                 Discord
                             </button>
-                            <button type="button" className="bg-blue-800 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-blue-900 focus:outline-none">
+                            <button type="button" className="bg-blue-700 text-white px-4 py-2 rounded-md flex items-center justify-center hover:bg-blue-800 focus:outline-none">
                                 <img src="logos/steam.png" alt="Discord Icon" className="w-6 h-6 mr-2 object-contain" />
                                 Steam
                             </button>
